3ef33101f2d5678e022da5326c9e4c4a71280afc
mfcsds <fathur.cs.ds@gmail.com>
2024-07-25 11:36:09 +0700

    Update page.tsx

--- app/manageaccount/page.tsx
+++ app/manageaccount/page.tsx
@@ -1,34 +1,37 @@
+"use client";
 
-'use client'
-
-import { Button } from '@/components/ui/button'
-import React from 'react'
-import { useRouter } from 'next/navigation'
-import {
-    Plus
-}
-from "lucide-react"
-import TableManageAccount from '@/components/TableManageAccount'
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { Plus } from "lucide-react";
+import TableManageAccount from "@/components/TableManageAccount";
 
 const page = () => {
-  const router = useRouter()
+  const router = useRouter();
 
-  const navigateTo = (path:string) => {
-      router.push(path)
-  }
-  
+  const navigateTo = (path: string) => {
+    router.push(path);
+  };
+
   return (
-    <div className='px-5 py-5 flex flex-col w-full'>  
+    <div className="px-5 py-5 flex flex-col w-full items-center">
       <div className="flex flex-row-reverse mb-5">
-        <Button  className="hover:text-white hover:bg-violet-800" variant="secondary" onClick={()=> navigateTo("./manageaccount/createnewuser")}>
-            <span><Plus className='w-3 h-3 mr-2'></Plus></span> Add New Account
+        <Button
+          className="hover:text-white hover:bg-violet-800"
+          variant="secondary"
+          onClick={() => navigateTo("./manageaccount/createnewuser")}
+        >
+          <span>
+            <Plus className="w-3 h-3 mr-2"></Plus>
+          </span>{" "}
+          Add New Account
         </Button>
       </div>
-      <div className='flex flex-row'>
+      <div className="flex flex-row">
         <TableManageAccount></TableManageAccount>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
