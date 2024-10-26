f665d5af1d0212a178c74e2e284517ba8ffca5b7
mfcsds <fathur.cs.ds@gmail.com>
2024-10-26 10:05:23 +0700

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,7 +5,6 @@ import Sidebar from "@/components/Sidebar";
 import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
   title: "Senusa-App",
   description: "Software Penilaian Gen untuk Bangsa",
